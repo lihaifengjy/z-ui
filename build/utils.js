78f2f23e5cfd190e1361c5fa77b25e3b19d5a570
unknown <lihaifeng@wsn.cn>
2019-03-05 17:53:36 +0800

    修改

--- build/utils.js
+++ build/utils.js
@@ -31,38 +31,50 @@ exports.cssLoaders = function (options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
-
+    // const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+    const loaders = []
     if (loader) {
-      loaders.push({
+      loaders = [{
         loader: loader + '-loader',
         options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap
+            sourceMap: options.sourceMap
         })
-      })
+      }]
+      // loaders.push({
+      //   loader: loader + '-loader',
+      //   options: Object.assign({}, loaderOptions, {
+      //     sourceMap: options.sourceMap
+      //   })
+      // })
     }
 
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
+      let extractLoader = {
+        loader: MiniCssExtractPlugin.loader,
+        options: {}
+      }
+      return [extractLoader, 'css-loader'].concat(['postcss-loader'], loaders)
     } else {
-      return ['vue-style-loader'].concat(loaders)
+      return ['vue-style-loader', 'css-loader'].concat(['postcss-loader'], loaders)
     }
   }
 
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
-    postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+        postcss: generateLoaders(),
+        less: generateLoaders('less'),
+        sass: generateLoaders('sass', { indentedSyntax: true }),
+        scss: generateLoaders('sass').concat({
+          loader: 'sass-resources-loader',
+          options: {
+              resources: path.resolve(__dirname, '../src/assets/styles/common.scss')
+          }
+        }),
+        stylus: generateLoaders('stylus'),
+        styl: generateLoaders('stylus')
   }
 }
 
